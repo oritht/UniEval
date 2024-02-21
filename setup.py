ebfe59a67f604b448534c7d83af10ed0315545f3
oritht <oritht@il.ibm.com>
2024-02-21 11:41:28 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='UniEval',
     version='1.0.0',
-    packages=['uni_eval'],
+    packages=['uni_eval', 'uni_eval.metric'],
     url='',
     license='',
     author='Ming Zhong',
